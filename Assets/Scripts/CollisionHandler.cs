a8599351327de706c3f7da5604a567aa8ab5726f
oliver2-thomson <Oliver2.Thomson@live.uwe.ac.uk>
2024-04-29 21:47:51 +0100

    still cant get it working

--- Assets/Scripts/CollisionHandler.cs
+++ Assets/Scripts/CollisionHandler.cs
@@ -1,32 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
 
-public class PlayerInteraction : MonoBehaviour
+public class CollisionHandler : MonoBehaviour
 {
-    public OpenDoor Left_Door;
-    public OpenDoor Right_Door;
     // Start is called before the first frame update
     void Start()
     {
-
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.F)) 
-        {
-            Left_Door.ToggleDoor();
-            Right_Door.ToggleDoor();
-            
-        }
+        
     }
-
     private void OnCollisionStay(Collision collision)
     {
         print("collided");
     }
-
 }
